93394561831a4be2f2fbe87aabac324a3a8b9a5e
itsSkanda <itsabishekskanda@gmail.com>
2026-03-15 19:48:53 +0530

    Refactor palindrome checker and rename class
    Refactored palindrome checking methods and updated class name.

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,53 @@
 import java.util.Scanner;
+import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
 
-public class UseCase4PalindromeCheckerApp {
+public class UseCase13PalindromeCheckerApp {
+
+    public static boolean checkUsingArray(String input) {
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static boolean checkUsingStack(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkUsingDeque(String input) {
+        Deque<Character> deque = new LinkedList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
 
@@ -14,26 +61,21 @@ public class UseCase4PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        char[] characters = input.toCharArray();
+        long start1 = System.nanoTime();
+        boolean result1 = checkUsingArray(input);
+        long end1 = System.nanoTime();
 
-        int start = 0;
-        int end = characters.length - 1;
-        boolean isPalindrome = true;
+        long start2 = System.nanoTime();
+        boolean result2 = checkUsingStack(input);
+        long end2 = System.nanoTime();
 
-        while (start < end) {
-            if (characters[start] != characters[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
+        long start3 = System.nanoTime();
+        boolean result3 = checkUsingDeque(input);
+        long end3 = System.nanoTime();
 
-        if (isPalindrome) {
-            System.out.println("The given string is a Palindrome.");
-        } else {
-            System.out.println("The given string is NOT a Palindrome.");
-        }
+        System.out.println("Array Method Result: " + result1 + " | Time: " + (end1 - start1) + " ns");
+        System.out.println("Stack Method Result: " + result2 + " | Time: " + (end2 - start2) + " ns");
+        System.out.println("Deque Method Result: " + result3 + " | Time: " + (end3 - start3) + " ns");
 
         scanner.close();
     }
